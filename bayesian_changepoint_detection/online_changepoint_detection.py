5e1c1f4c0f2b9ad6526e8f2769e3cbf87296e3ae
Ravindra Kompella <kmsravindra@yahoo.com>
2020-06-08 11:20:20 +0530

    Added new distributions, hazard function
    1. Added the case when it is a normal distribution, Mean is unknown but Precision is known
    2. Added Poisson distribution
    3. Added alternate hazard function ( assuming it is a function of time and is normally distributed with given mean and standard deviation)

--- bayesian_changepoint_detection/online_changepoint_detection.py
+++ bayesian_changepoint_detection/online_changepoint_detection.py
@@ -40,6 +40,16 @@ def constant_hazard(lam, r):
     return 1/lam * np.ones(r.shape)
 
 
+def gaussian_hazard(mu, sigma, r):
+    # mu and sigma are the hyperparameters of the cecum event happening. In our case 12000 frames correspond to 6.5min where cecum is typically observed
+    # Hazard function = pdf/survival = pdf/(1-cdf)
+    # Greater run length will have greater hazard. So we use stats.norm.pdf(r) directly which will return an array of hazard values for each run length
+    pdf = stats.norm.pdf(r,mu,sigma) # Computes probability for each runlength starting from 0 to t
+    survival = 1-stats.norm.cdf(r,mu,sigma) # Computes survival for each runlength starting from 0 to t
+    hazard_arr =  pdf / survival # Computes hazard value for each runlength starting from 0 to t
+    #hazard_arr = hazard*np.ones(r.shape) # Apply the same hazard for all vaules of column t in R[] ---> Not reqired since the previous line itself is an array of r.shape values
+    return hazard_arr
+
 class StudentT:
     def __init__(self, alpha, beta, kappa, mu):
         self.alpha0 = self.alpha = np.array([alpha])
@@ -65,3 +75,37 @@ class StudentT:
         self.kappa = kappaT0
         self.alpha = alphaT0
         self.beta = betaT0
+
+class NormalKnownPrecision:
+    # Normal --> use this when variance is known and mean is unknown / changing for the incoming data
+    def __init__(self, mu, prec):
+        self.mu0 = self.mu = np.array([mu])
+        self.prec0 = self.prec = np.array([prec])
+
+    def pdf(self, data):
+        return stats.norm.pdf(data,self.mu, 1/self.prec + 1)
+
+    def update_theta(self, data, t):
+        offsets = np.arange(1, t+2) # To calculate new mean params at each node of the next time step,  we need to multiply each of the previous mean value in self.mu array (2u1+data/3, 3u2+data/4 etc.,), we have offsets
+        muT0 = np.concatenate((self.mu0, (offsets*self.mu + data)/(offsets+1)))
+        precT0 = np.concatenate((self.prec0, self.prec+self.prec0))
+        #precT0 = np.concatenate((self.prec0,  math.sqrt(t / ( (t-1)/self.prec**2 + (data-newMu)*(data-self.mu))) ))
+
+        self.mu = muT0
+        self.prec = precT0
+
+
+class Poisson:
+    def __init__(self, k, theta):
+        self.k0 = self.k = np.array([k])
+        self.theta0 = self.theta = np.array([theta])
+
+    def pdf(self, data):
+        return stats.nbinom.pmf(data,self.k, 1/(1+self.theta))
+
+    def update_theta(self, data):
+        kT0 = np.concatenate((self.k0, self.k+data))
+        thetaT0 = np.concatenate((self.theta0, self.theta/(1+self.theta)))
+
+        self.k = kT0
+        self.theta = thetaT0
